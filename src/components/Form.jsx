4d838e4ce8189fbdf13de853d6d68fd6791a8b35
Lorenzo Costa <lorenzocostasan94@gmail.com>
2022-08-03 20:32:54 -0300

    funcao login FORM.JSX

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,12 +5,47 @@ import { registerSchema } from './schema/Register';
 // utilitários
 import { useFormik } from 'formik';
 import { useState } from 'react';
+// import Airtable from 'airtable';
 import "../styles/FormLogin.module.scss"
 import "../styles/FormLogin.module.css"
+import axios from 'axios'; 
+
 
 
 
 export const Form = () => {
+
+  axios.get('https://api.airtable.com/v0/app6wyVEK4ZQbbAzm/usuarios?maxRecords=3&view=Grid%20view',
+  {
+    headers: {
+        'Authorization': 'Bearer key83wTk6Qka7Kibs'
+    }
+}).then(resp => {
+  console.log(resp.data.records)
+
+  let usersList = resp.data.records
+
+  usersList.filter(ids => {
+    console.log("cpf :" + ids.fields.cpf)
+    console.log("emails :" + ids.fields.email)
+    console.log("ids :"+ ids.id)
+
+    return true
+  })
+
+})
+    // const [users, setUsers] = useState([])
+
+    // base('usuarios')
+    //     .select({ view: 'Grid view' })
+    //     .eachPage((records, fetchNextPage) => {
+    //     setUsers(records)  
+    //     fetchNextPage();        
+    //     });
+
+        
+
+        
   const [showErrors, setShowErrors] = useState(false);
 
   const { values, errors, handleChange, handleSubmit } = useFormik({
@@ -33,6 +68,8 @@ export const Form = () => {
   // FUNCAO RESPONSAVEL POR VERIFICAR EMAIL E CPF NA BASE,E REDIRECIONAR PARA A LISTA CRIADA, OU PARA A CRIACAO DE LISTA.
 
   function login() {
+
+    // console.log(users)
 
     alert("logou no sistema")
   }
