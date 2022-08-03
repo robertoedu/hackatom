f869a04dffa87c15c8ad784c69913ab44dbebd42
Lexkogake <lexkogakedev@gmail.com>
2022-08-02 21:08:23 -0300

    incluido a rota, list, incluido a busca do banco

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from '../pages/Login';
+import { PageList } from '../pages/PageList';
 
 export const MainRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/list" element={<PageList />} />
       </Routes>
     </BrowserRouter>
   );
