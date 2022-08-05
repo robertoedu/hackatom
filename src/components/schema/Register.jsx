df532f130dd5b2250dbcbfe14fab99a5fa2b7838
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-08-05 14:08:55 -0300

    refatoração no register

--- src/components/schema/Register.jsx
+++ src/components/schema/Register.jsx
@@ -1,14 +1,10 @@
 import * as yup from 'yup';
 
 export const registerSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email('E-mail inválido!')
-    .required('E-mail obrigatório!'),
+  email: yup.string().email('E-mail inválido!').required('E-mail obrigatório!'),
 
   cpf: yup
     .string()
-    .min(11, 'O CPF precisa ter no mínimo 11 caracteres!')
-    .max(11, "O CPF tem No Maximo 11 caracteres!")
+    .min(14, 'O CPF precisa ter no mínimo 14 caracteres!')
     .required('CPF é obrigatório!'),
 });
